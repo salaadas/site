ff1d5dce1782164a1fb41ee26375f46e27b1a115
salaadas <carbonfibersweeper@gmail.com>
2023-03-29 09:28:04 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     app.setBaseViewsDir(join(__dirname, '..', 'templates')); // for view engine templates
     app.setViewEngine('pug');
 
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
